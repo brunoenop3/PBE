8132d44b21582c97b8974c30d86edbd3261b1285
brunoenop3 <amorosa.poderoso0d@icloud.com>
2025-02-27 16:11:57 +0100

    cambio de libreria a mfrc522

--- PUZZLE 1.rb
+++ PUZZLE 1.rb
@@ -1,3 +1,17 @@
-# RFID MIFARE PUZZLE 1
-# We have to read the uid of the car in capital letters
-puts "hello"
+require 'mfrc522'
+
+reader = MFRC522.new
+
+puts 'Acerca una tarjeta al lector...'
+
+# Espera hasta que una tarjeta esté presente
+reader.picc_request(MFRC522::PICC_REQA)
+
+# Lee la UID de la tarjeta
+uid = reader.picc_select
+
+if uid
+  puts "UID de la tarjeta: #{uid.map { |byte| byte.to_s(16).upcase }.join(':')}"
+else
+  puts 'No se pudo leer la UID de la tarjeta.'
+end
