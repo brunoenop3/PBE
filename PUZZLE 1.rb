3444522b730204fefd67120b6a114270c415ef22
brunoenop3 <amorosa.poderoso0d@icloud.com>
2025-03-02 02:15:34 +0100

    cambio en argumentos de la funcion

--- PUZZLE 1.rb
+++ PUZZLE 1.rb
@@ -2,7 +2,7 @@ require 'mfrc522'
 
 mfrc = MFRC522.new
 
-mfrc.picc_request    # Detecta si hay una tarjeta
-uid = mfrc.picc_select  # Obtiene la UID
-
-puts uid.map { |byte| byte.to_s(16).rjust(2, '0') }.join(':') if uid
+if mfrc.picc_request(1)  # Se pasa un argumento al método
+  uid = mfrc.picc_select
+  puts uid.map { |byte| byte.to_s(16).rjust(2, '0') }.join(':') if uid
+end
