d8a6364a86079690dd308a0506af45c89b0d3b42
brunoenop3 <amorosa.poderoso0d@icloud.com>
2025-03-02 10:29:55 +0100

    no lee

--- PUZZLE 1.rb
+++ PUZZLE 1.rb
@@ -2,7 +2,12 @@ require 'mfrc522'
 
 mfrc = MFRC522.new
 
-if mfrc.picc_request(1)  # Se pasa un argumento al método
-  uid = mfrc.picc_select
-  puts uid.map { |byte| byte.to_s(16).rjust(2, '0') }.join(':') if uid
+loop do
+  if mfrc.picc_request(MFRC522::PICC_REQIDL)
+    uid = mfrc.picc_select
+    if uid
+      puts "Tarjeta detectada: " + uid.map { |byte| byte.to_s(16).rjust(2, '0') }.join(':')
+    end
+  end
+  sleep 1  # Espera un poco antes de volver a checar
 end
